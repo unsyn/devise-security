bd4c4162b2b6608afc5b4eb98e1d7591c3914315
Johannes <johannes@unsyn.com>
2019-05-15 21:17:07 +0200

    Application specific captcha-incorrect handling.

--- lib/devise-security/patches/controller_captcha.rb
+++ lib/devise-security/patches/controller_captcha.rb
@@ -14,7 +14,11 @@ module DeviseSecurity::Patches
       return if valid_captcha_if_defined?(params[:captcha])
 
       flash[:alert] = t('devise.invalid_captcha') if is_navigational_format?
-      respond_with({}, location: url_for(action: :new))
+
+      # app specific: redirect to consumer login on capture failures if coming from consumer login.
+      action = (params[:user] && params[:user][:sign_in_type] == 'consumer') ? :new_consumer : :new
+
+      respond_with({}, location: url_for(action: action))
     end
   end
 end
